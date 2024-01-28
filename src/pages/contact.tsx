ce1ca33934cfc383576973adf88fb20c02878318
Mario <mariovparaschiv@gmail.com>
2024-01-28 02:22:36 +0000

    change contact icon

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 import Typography from '~/components/typography';
-import { ContactIcon } from 'lucide-react';
 import { Page } from '~/components/layout';
+import { Contact2 } from 'lucide-react';
 
 export const path = '/contact';
 export const element = Contact;
@@ -9,7 +9,7 @@ export const order = 4;
 function Contact() {
 	return <Page section='Contact' className='p-0 flex min-h-screen w-screen items-center justify-center'>
 		<div className='flex items-center gap-8 flex-col m-auto animate-in fade-in-0 zoom-in-105 slide-in-from-bottom-8 duration-500 bg-gradient-to-br from-white to-neutral-500 bg-clip-text text-transparent'>
-			<ContactIcon className='text-neutral-400' size={256} strokeWidth={1} />
+			<Contact2 className='text-neutral-400' size={256} strokeWidth={1.5} />
 			<Typography tag='h1' className='font-semibold'>
 				Under construction.
 			</Typography>
