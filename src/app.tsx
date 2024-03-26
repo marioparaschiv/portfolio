b9c57c1277345237295e9d67a71d7718131e737c
Mario <me@marioparaschiv.com>
2024-03-26 13:00:13 +0000

    fix type

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import * as Pages from '~/pages';
 
-const routes = Object.values(Pages).map(({ path, element: Component }: Pages.Page) => ({ path, element: <Component /> }));
+const routes = Object.values(Pages).map(({ path, element: Component }: Page) => ({ path, element: <Component /> }));
 const router = createHashRouter(routes);
 
 function App() {
