472e84b1ee78cfd0caf1fad2cf626e323001c816
Mario <me@marioparaschiv.com>
2024-07-29 17:31:41 +0100

    🔧 Fix BrowserRouter on Vercel

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import * as Pages from '~/pages';
 
 const routes = Object.values(Pages).map(({ path, element: Component }: Page) => ({ path, element: <Component /> }));
-const router = createHashRouter(routes);
+const router = createBrowserRouter(routes);
 
 function App() {
 	return (
